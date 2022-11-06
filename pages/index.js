5a03290b7b0fc70fadee53f7ad014207d22fbcf4
sphiwe <sphiwe@thedigitalacademy.co.za>
2022-11-06 16:42:49 +0200

    Persist the cart across page loads

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import prisma from 'lib/prisma'
 import { getProducts } from 'lib/data.js'
-import { useState } from 'react'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import * as localForage from 'localforage'
 
 export default function Home({ products }) {
   const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    localForage.getItem('cart', function (err, value) {
+      if (value) {
+        setCart(value)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    localForage.setItem('cart', cart)
+  }, [cart])
+
   return (
     <div>
       <Head>
@@ -16,6 +30,7 @@ export default function Home({ products }) {
 
       <div className=''>
         <h1 className='mt-10 font-extrabold text-4xl text-center'>Shop</h1>
+
         {cart.length > 0 && (
           <div className='mt-20 sm:mx-auto max-w-4xl mx-10 border-2 border-black'>
             <h3 className='py-2 font-extrabold text-2xl text-center'>
@@ -36,40 +51,51 @@ export default function Home({ products }) {
                   quantity: {item.quantity}
                 </div>
               </div>
-            ))}{' '}
+            ))}
+             <button
+              className='mx-auto bg-black text-white px-3 py-1 my-4 text-sm justify-center flex'
+              onClick={() => setCart([])}
+            >
+              Clear cart
+            </button>
+            {' '}
           </div>
+          
         )}
+         
+
         <div className='mt-20 sm:mx-auto max-w-4xl mx-10'>
           {products.map((product) => (
             <div className='mb-4 grid sm:grid-cols-2' key={product.id}>
               <div>
-                <Image src={`/` + product.image} width={'600'} height={'600'} alt={'photo'} />
+                <Image src={`/` + product.image} width={'600'} height={'600'} />
               </div>
               <div className='sm:ml-10 mb-20 sm:mb-0'>
                 <h2 className='text-3xl font-extrabold'>{product.title}</h2>
                 <h3 className='text-2xl font-extrabold mb-4'>
                   ${product.price / 100}
                 </h3>
-                <button className='mb-4 mx-auto bg-black text-white px-3 py-1 text-lg'
-               onClick={() => {
-                const itemsInCartWithThisId = cart.filter((item) => {
-                  return item.product.id === product.id
-                })
+                <button
+                  className='mb-4 mx-auto bg-black text-white px-3 py-1 text-lg'
+                  onClick={() => {
+                    const itemsInCartWithThisId = cart.filter((item) => {
+                      return item.product.id === product.id
+                    })
 
-                if (itemsInCartWithThisId.length > 0) {
-                  setCart([
-                    ...cart.filter((item) => {
-                      return item.product.id !== product.id
-                    }),
-                    {
-                      product: itemsInCartWithThisId[0].product,
-                      quantity: itemsInCartWithThisId[0].quantity + 1,
-                    },
-                  ])
-                } else {
-                  setCart([...cart, { product, quantity: 1 }])
-                }
-              }}
+                    if (itemsInCartWithThisId.length > 0) {
+                      setCart([
+                        ...cart.filter((item) => {
+                          return item.product.id !== product.id
+                        }),
+                        {
+                          product: itemsInCartWithThisId[0].product,
+                          quantity: itemsInCartWithThisId[0].quantity + 1,
+                        },
+                      ])
+                    } else {
+                      setCart([...cart, { product, quantity: 1 }])
+                    }
+                  }}
                 >
                   Add to cart
                 </button>
